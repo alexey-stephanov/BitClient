b3de01a33a7590a3b78724cc10d1cfd70a78d3a2
alexey-stephanov <48553755+alexey-stephanov@users.noreply.github.com>
2020-12-11 17:49:01 +0300

    ViewBinding has been added.

--- app/src/main/java/com/example/bitclient/ui/view/activities/MainActivity.kt
+++ app/src/main/java/com/example/bitclient/ui/view/activities/MainActivity.kt
@@ -1,24 +1,20 @@
 package com.example.bitclient.ui.view.activities
 
-import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.browser.customtabs.CustomTabsIntent
+import androidx.fragment.app.FragmentActivity
+import androidx.navigation.NavController
 import androidx.navigation.Navigation
-import androidx.navigation.ui.NavigationUI
 import com.example.bitclient.R
+import com.example.bitclient.databinding.ActivityMainBinding
+import com.example.bitclient.ui.view.fragments.SignInFragmentDirections
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : FragmentActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        setTheme(R.style.Theme_BitClient)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        val navController = Navigation.findNavController(this, R.id.nav_host_fragment)
-
-        val url = "https://bitbucket.org/site/oauth2/authorize?client_id=cDXfEkaZFs8aSvnVgT"
-        val builder = CustomTabsIntent.Builder()
-        val customTabsIntent = builder.build()
-        customTabsIntent.launchUrl(this, Uri.parse(url))
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
--- app/src/main/java/com/example/bitclient/ui/view/fragments/SignInFragment.kt
+++ app/src/main/java/com/example/bitclient/ui/view/fragments/SignInFragment.kt
@@ -1,7 +1,30 @@
 package com.example.bitclient.ui.view.fragments
 
+import android.net.Uri
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.browser.customtabs.CustomTabsIntent
 import androidx.fragment.app.Fragment
+import com.example.bitclient.R
+import com.example.bitclient.databinding.FragmentSignInBinding
+import com.example.bitclient.ui.view.fragments.viewbinding.viewBinding
 
-class SignInFragment : Fragment() {
+class SignInFragment : Fragment(R.layout.fragment_sign_in) {
 
+    private val fragmentSignInBinding by viewBinding(FragmentSignInBinding::bind)
+    private val clientId = "cDXfEkaZFs8aSvnVgT"
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        fragmentSignInBinding.buttonSignInEnter.setOnClickListener {
+            val url = "https://bitbucket.org/site/oauth2/authorize?client_id=$clientId"
+            val builder = CustomTabsIntent.Builder()
+            val customTabsIntent = builder.build()
+            customTabsIntent.launchUrl(requireContext(), Uri.parse(url))
+        }
+    }
 }
